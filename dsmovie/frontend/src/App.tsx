ba935ff68cdbeb4d1de169eb6d0131ff203b2293
Junior Mazalli <junior.mazalli@afgbrasil.com.br>
2022-03-09 09:29:21 -0300

    Security config

--- dsmovie/frontend/src/App.tsx
+++ dsmovie/frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import Listing from 'pages/Listing';
 import Form from 'pages/Form';
+import Form2 from 'pages/Form2';
 import Navbar from "components/Navbar";
 
 function App() {
@@ -15,6 +16,11 @@ function App() {
         <Route path="/" element={<Listing />} />
         <Route path="/form">
           <Route path=":movieId" element={<Form />} />
+        </Route>
+
+        <Route path="/" element={<Listing />} />
+        <Route path="/form2">
+          <Route path=":movieId" element={<Form2 />} />
         </Route>
       </Routes>
     </BrowserRouter>
