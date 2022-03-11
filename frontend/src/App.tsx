5eb4bc9011b29e23043177111375f342ce0cc99d
Junior Mazalli <junior.mazalli@afgbrasil.com.br>
2022-03-11 11:01:22 -0300

    First request

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,6 @@ import {
 } from "react-router-dom";
 import Listing from 'pages/Listing';
 import Form from 'pages/Form';
-import Form2 from 'pages/Form2';
 import Navbar from "components/Navbar";
 
 function App() {
@@ -18,10 +17,7 @@ function App() {
           <Route path=":movieId" element={<Form />} />
         </Route>
 
-        <Route path="/" element={<Listing />} />
-        <Route path="/form2">
-          <Route path=":movieId" element={<Form2 />} />
-        </Route>
+
       </Routes>
     </BrowserRouter>
   );
